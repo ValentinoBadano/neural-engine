f0ff885120d04da4c5814c50430ce11923a3a891
ValentinoBadano <valentinobadano@gmail.com>
2023-09-15 19:54:25 -0300

    clase neuralnetwork con grafico

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 from layer import *
 from activation_layer import *
 from cost_functions import quadratic_error, quadratic_error_derivative
+import matplotlib.pyplot as plt 
 
 class NeuralNetwork(): 
     def __init__(self, layers):
@@ -17,8 +18,9 @@ class NeuralNetwork():
             y = l.propagate(y)
         return y;
 
-    def train(self, x_train, y_train, epochs, learning_rate):
+    def train(self, x_train, y_train, epochs, learning_rate, verbose = True, graph = False):
         for i in range(epochs):
+            error_promedio = 0
             for x, y in zip(x_train, y_train):
                 output = x
 
@@ -26,8 +28,22 @@ class NeuralNetwork():
                 for l in self.layers:
                     output = l.propagate(output)
 
-                error = quadratic_error_derivative(y, output)
+                error_promedio += quadratic_error(y, output)   # para registro
+                error = quadratic_error_derivative(y, output) 
 
                 # prop. hacia atrás
                 for l in reversed(self.layers):
                     error = l.retropropagate(error, learning_rate)
+
+            # imprime el error si el verbose está en true        
+            if verbose:
+                error_promedio /= len(y_train)
+                print("Epoch:", i, "Loss:", error_promedio)
+
+            if graph:
+                plt.plot(i, error_promedio, 'ro', color='blue', markersize=0.2)
+                
+        if graph:
+            plt.xlabel('Training')
+            plt.ylabel('Loss')
+            plt.show()
