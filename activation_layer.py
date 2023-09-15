b2dde4ed19aa635f93232dc4b9abae8eacef2e90
ValentinoBadano <valentinobadano@gmail.com>
2023-09-15 19:54:12 -0300

    overflow y comentarios

--- activation_layer.py
+++ activation_layer.py
@@ -6,10 +6,12 @@ class ActivationLayer(object):
 		self.activation_function_derivative = activation_function_derivative
 
 	def propagate(self, input):
+		# Dada una entrada calcula la salida de la función de activación
 		self.input = input
 		self.output = self.activation_function(input)
 		return self.output 
 
 	def retropropagate(self, error, lrate):
+		# el learning rate no se utiliza en este tipo de capa ya que no hay nada para aprender
 		de_dx = self.activation_function_derivative(self.input) * error
 		return de_dx
